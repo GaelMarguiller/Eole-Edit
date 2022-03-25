0ee7c7afaafec2e4f77d19f26bc15cf9a305620d
GMarguiller <gael.marguiller@supinternet.fr>
2022-03-25 16:29:20 +0100

    Fix back-end

--- back-end/routes/uploadRoute.ts
+++ back-end/routes/uploadRoute.ts
@@ -10,21 +10,12 @@ mongoose.connection.on('connected', () => {
 });
 
 router.post('/', async (req, res) => {
-    console.log(req);
-    console.log(req.body);
     const { title, size, type } = req.body;
     if (!title || !size || !type) {
         return res
             .status(400)
             .send('File invalid');
     }
-
-    const videoFile = await new VideoFile();
-    videoFile.title = title;
-    videoFile.type = type;
-    videoFile.size = size;
-
-    return videoFile;
 });
 
 export default router;
